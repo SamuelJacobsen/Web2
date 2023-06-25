18448bbb2cace8a5612447d513c366905705d2a8
Samuel Penha Jacobsen <samuelpj1606@gmail.com>
2023-06-25 19:56:23 -0300

    configuração inicial Controller e rotas dos usuarios

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,12 @@ app.use(express.json())
 //pasta para as imagens
 app.use(express.static('public'))
 
+//Routes
+const UserRoutes = require('./routes/UserRoutes')
+app.use('/users', UserRoutes)
+
+
+
 
 app.listen(3000, () => {
     console.log('Servidor está rodando na porta 3000');
