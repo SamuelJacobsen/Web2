40564249c36cd0c47a6f25ed54b54dbe4e09b2b0
Samuel Penha Jacobsen <samuelpj1606@gmail.com>
2023-06-27 10:56:18 -0300

    Verificação do token do usuário

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,12 +2,15 @@ const router = require('express').Router()
 
 const UserController = require('../controllers/UserController')
 
+//middlewares
+const verifyToken = require('../helpers/verify-token')
+
 router.post('/register', UserController.register)
-
 router.post('/login', UserController.login)
-
 router.get('/checkuser', UserController.checkUser)
-
 router.get('/:id', UserController.getUserById)
+
+//rota de atualizacao portanto protegida
+router.patch('/edit/:id', verifyToken, UserController.editUser)
 
 module.exports = router
